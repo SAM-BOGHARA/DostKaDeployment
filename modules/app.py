05ff42bcaab2339bb8b3f9744f83d9862e9016b7
Shubham Boghara <shubhamboghara444@gmail.com>
2022-09-27 23:29:20 +0530

    Procfile added

--- modules/app.py
+++ modules/app.py
@@ -1,4 +1,3 @@
-from crypt import methods
 import MainRepo
 from flask import Flask, render_template, redirect, session, send_file, request
 from flask_mail import Mail, Message
@@ -9,10 +8,10 @@ import os
 app = Flask(__name__)
 
 
-if(os.environ.get('ENV') == "Production"):
-    app.config.from_object("config.ProductionConfig")
-else:
-    app.config.from_object("config.DevelopmentConfig")
+# if(os.environ.get('ENV') == "Production"):
+#     app.config.from_object("config.ProductionConfig")
+# else:
+#     app.config.from_object("config.DevelopmentConfig")
 
 mail = Mail(app)
 db = MainRepo.Repo(app.config)
